ce2953d41b5cbe534ba047051c297815c5fee38a
양희승 <heeseung0223@naver.com>
2024-03-10 14:17:40 +0900

    chore: init DB

---
+++ db/SELECT.sql
@@ -0,0 +1,14 @@
+-- O
+SELECT * FROM users;
+SELECT * FROM parties;
+SELECT * FROM guests;
+SELECT * FROM hints;
+SELECT * FROM missions;
+SELECT * FROM topics;
+
+-- X
+SELECT * FROM hint_status;
+SELECT * FROM mission_status;
+SELECT * FROM chats;
+SELECT * FROM messages;
+SELECT * FROM results;
