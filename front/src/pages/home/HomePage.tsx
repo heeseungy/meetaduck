bb368bcc6c426ea44348d866cf2247d8f1eeec73
안준선 <joonsun1006@naver.com>
2024-03-13 10:40:36 +0900

    feat: Home debugging routing

--- front/src/pages/home/HomePage.tsx
+++ front/src/pages/home/HomePage.tsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
   return (
-    <h1>Hi I am Dummy Home Page</h1>
+    <>
+      <h1>Hi I am Dummy Home Page</h1>
+      <p><Link to="/login">로그인 페이지</Link>로 이동</p>
+      <p><Link to="/result">결과 페이지</Link>로 이동</p>
+    </>
   )
 }
 
