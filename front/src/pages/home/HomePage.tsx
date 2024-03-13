dcfec076e44300cd4fcbdcb4e0951f734f90fa4e
안준선 <joonsun1006@naver.com>
2024-03-13 12:10:10 +0900

    feat: 홈페이지에 개발용 링크 추가

--- front/src/pages/home/HomePage.tsx
+++ front/src/pages/home/HomePage.tsx
@@ -5,6 +5,10 @@ function HomePage() {
     <>
       <h1>Hi I am Dummy Home Page</h1>
       <p><Link to="/login">로그인 페이지</Link>로 이동</p>
+      <p><Link to="/mission">미션 페이지</Link>로 이동</p>
+      <p><Link to="/chatting">채팅 페이지</Link>로 이동</p>
+      <p><Link to="/hint">힌트 페이지</Link>로 이동</p>
+      <p><Link to="/vote">투표 페이지</Link>로 이동</p>
       <p><Link to="/result">결과 페이지</Link>로 이동</p>
     </> 
   )
