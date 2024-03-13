b299ea0265d60327c3a4178d10a15e25e8b25696
안준선 <joonsun1006@naver.com>
2024-03-13 09:48:24 +0900

    feat: add ResultPage routing

--- front/src/pages/Route.tsx
+++ front/src/pages/Route.tsx
@@ -2,10 +2,12 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import HomePage from './home/HomePage';
 import LoginPage from './login/LoginPage';
+import ResultPage from './result/ResultPage';
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
   { path: '/login', element: <LoginPage /> },
+  { path: '/result', element: <ResultPage /> },
 ]);
 
 function Route() {
---
+++ front/src/pages/result/ResultPage.tsx
@@ -0,0 +1,7 @@
+function ResultPage() {
+  return (
+    <h1>Hi I am Result Page</h1>
+  )
+}
+
+export default ResultPage;
