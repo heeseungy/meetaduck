0d7ff4647803c54adf89720a0f2e37d5f88f2dc6
안준선 <joonsun1006@naver.com>
2024-03-13 14:20:46 +0900

    feat: Navbar 정렬

--- front/src/components/navbar/MainNav.tsx
+++ front/src/components/navbar/MainNav.tsx
@@ -1,16 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import classes from '../../styles/MainNav.module.css';
 
 function MainNav() {
   return (
     <>
-      <nav>
-        <div>아래에 있는 것들은 네비게이션 바 입니다.</div>
-        <ul>
-          <li><Link to="/mission">미션</Link></li>
-          <li><Link to="/chatting">채팅</Link></li>
-          <li><Link to="/hint">힌트</Link></li>
-          <li><Link to="/result">결과</Link></li>
-        </ul>
+      <nav className={classes.wrapper}>
+        {/* <div>아래에 있는 것들은 네비게이션 바 입니다.</div> */}
+        <div><Link to="/mission">미션</Link></div>
+        <div><Link to="/chatting">채팅</Link></div>
+        <div><Link to="/hint">힌트</Link></div>
+        <div><Link to="/result">결과</Link></div>
       </nav>
     </>
   )
