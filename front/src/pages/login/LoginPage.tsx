a0bbe28cdedd4c8056a230b44a0241c11c1d151d
안준선 <joonsun1006@naver.com>
2024-03-13 17:03:25 +0900

    feat: loginPage styling

--- front/src/pages/login/LoginPage.tsx
+++ front/src/pages/login/LoginPage.tsx
@@ -1,10 +1,28 @@
 import styles from '../../styles/LoginPage.module.css';
 
 function LoginPage() {
+  const loginHandler = () => {
+    console.log('카카오로 로그인하기');
+  };
+
   return (
     <>
-      <h1>Login Page</h1>
-      <div className={styles.Box}>asdf</div>
+      <header className={styles.HeadFont}>
+        <div>
+          <span className={styles.HeadDark}>믿어, </span>
+          <span className={styles.HeadWhite}>덕.</span>
+        </div>
+        <div>
+          <span className={styles.HeadDark}>Meet, A </span>
+          <span className={styles.HeadWhite}>Duck.</span>
+        </div>
+      </header>
+      <main>
+        <p>나의 마니또는 누구일까요?</p>
+        <p>채팅과 미션을 통해 알아보세요!</p>
+        <div>이미지 공간입니다</div>
+      </main>
+      <button onClick={loginHandler}>카카오로 로그인하기</button>
     </>
   );
 }
