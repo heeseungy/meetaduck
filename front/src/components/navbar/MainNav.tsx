35b40b3a4c873452bc2330995be361372a17a284
안준선 <joonsun1006@naver.com>
2024-03-13 14:41:11 +0900

    feat: navbar

--- front/src/components/navbar/MainNav.tsx
+++ front/src/components/navbar/MainNav.tsx
@@ -1,18 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+import { ChartBar, ChatCircleDots, FlagBanner, Lightbulb } from '@phosphor-icons/react';
+
 import classes from '../../styles/MainNav.module.css';
 
 function MainNav() {
   return (
     <>
       <nav className={classes.wrapper}>
-        {/* <div>아래에 있는 것들은 네비게이션 바 입니다.</div> */}
-        <div><Link to="/mission">미션</Link></div>
-        <div><Link to="/chatting">채팅</Link></div>
-        <div><Link to="/hint">힌트</Link></div>
-        <div><Link to="/result">결과</Link></div>
+        <div>
+          <Link to="/mission">
+            <FlagBanner size={32} />
+          </Link>
+        </div>
+        <div>
+          <Link to="/chatting">
+            <ChatCircleDots size={32} />
+          </Link>
+        </div>
+        <div>
+          <Link to="/hint">
+            <Lightbulb size={32} />
+          </Link>
+        </div>
+        <div>
+          <Link to="/result">
+            <ChartBar size={32} />
+          </Link>
+        </div>
       </nav>
     </>
-  )
+  );
 }
 
-export default MainNav
+export default MainNav;
