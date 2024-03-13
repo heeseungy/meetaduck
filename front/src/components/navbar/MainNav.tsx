d99eb48a819742c7313d3bacbbc7459ccd0f6a4f
안준선 <joonsun1006@naver.com>
2024-03-13 11:31:04 +0900

    feat: nav router setting

--- front/src/components/navbar/MainNav.tsx
+++ front/src/components/navbar/MainNav.tsx
@@ -4,6 +4,7 @@ function MainNav() {
   return (
     <>
       <nav>
+        <div>아래에 있는 것들은 네비게이션 바 입니다.</div>
         <ul>
           <li><Link to="#">미션</Link></li>
           <li><Link to="#">채팅</Link></li>
--- front/src/pages/Route.tsx
+++ front/src/pages/Route.tsx
@@ -3,11 +3,18 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './home/HomePage';
 import LoginPage from './login/LoginPage';
 import ResultPage from './result/ResultPage';
+import RootPage from './root/RootPage';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/login', element: <LoginPage /> },
-  { path: '/result', element: <ResultPage /> },
+  {
+    path: '/',
+    element: <RootPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/result', element: <ResultPage /> },
+      { path: '/login', element: <LoginPage /> },
+    ],
+  },
 ]);
 
 function Route() {
