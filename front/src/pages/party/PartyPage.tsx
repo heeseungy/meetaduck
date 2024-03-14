d29a719c13ff65f0240bdc10463921b0697a782c
안준선 <joonsun1006@naver.com>
2024-03-14 14:13:03 +0900

    feat: create Input component

--- front/src/pages/party/PartyPage.tsx
+++ front/src/pages/party/PartyPage.tsx
@@ -1,4 +1,5 @@
 import duckLogo from '@/assets/images/RubberDuckWithLogo.png';
+import Input from '@/components/commons/Input';
 import styles from '@/styles/Partyjoin.module.css';
 
 function PartyPage() {
@@ -6,6 +7,9 @@ function PartyPage() {
     <div className={styles.container}>
       <img src={duckLogo} alt="duckLogo" className={styles.marginTop} />
       <div className={`FontBasic FontL ${styles.joinTitle} `}>파티 참여 코드</div>
+      <div className={styles.marginTop}>
+        <Input />
+      </div>
     </div>
   );
 }
