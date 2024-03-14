7f4cce61cfe91864f376690922eb9c042b519bf2
강지수 <pwindx20@gmail.com>
2024-03-14 16:43:28 +0900

    feat: Hint Component

--- front/src/components/commons/Frame.tsx
+++ front/src/components/commons/Frame.tsx
@@ -1,10 +1,11 @@
-import { ReactNode } from 'react';
+// import { ReactNode } from 'react';
+import FrameTemplate from '@/assets/images/FrameTemplate.png';
+import styles from '@/styles/commons/Frame.module.css';
 
-import styles from '../../styles/Frame.module.css';
-
-type FrameProps = {
-  children: ReactNode;
-};
-const Frame = ({ children }: FrameProps) => <div className={styles.Frame}> {children} </div>;
+// type FrameProps = {
+//   children: ReactNode;
+// };
+// const Frame = ({ children }: FrameProps) => <div className={styles.Frame}> {children} </div>;
+const Frame = () => <img className={styles.Frame} src={FrameTemplate} alt="FrameTemplate" />;
 
 export default Frame;
---
+++ front/src/types/party.ts
@@ -0,0 +1,18 @@
+export enum StatusType {
+  'Todo' = 0,
+  'InProgress' = 1,
+  'Complete' = 2,
+}
+
+export interface Party {
+  partyId: number;
+  accessCode: string;
+  startTime: string;
+  endTime: string;
+  deleted: boolean;
+  userId: number;
+}
+
+export interface PartyStatus {
+  status: number;
+}
