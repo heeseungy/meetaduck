d978b9ae237b26653fd8e58f3f20e990c2d68a25
안준선 <joonsun1006@naver.com>
2024-03-14 11:37:17 +0900

    feat: party join title

--- front/src/pages/party/PartyPage.tsx
+++ front/src/pages/party/PartyPage.tsx
@@ -1,9 +1,13 @@
+import duckLogo from '@/assets/images/RubberDuckWithLogo.png';
+import styles from '@/styles/Partyjoin.module.css';
+
 function PartyPage() {
   return (
-    <>
-      <h1>Party Page</h1>
-    </>
-  )
+    <div className={styles.container}>
+      <img src={duckLogo} alt="duckLogo" className={styles.marginTop} />
+      <div className={`FontBasic FontL ${styles.joinTitle} `}>파티 참여 코드</div>
+    </div>
+  );
 }
 
-export default PartyPage
+export default PartyPage;
