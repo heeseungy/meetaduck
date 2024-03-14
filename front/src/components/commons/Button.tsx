8d034623e19f136211233a07ccd49a2c7e7c7bb8
안준선 <joonsun1006@naver.com>
2024-03-14 17:04:09 +0900

    feat: add ButtonProps type

--- front/src/components/commons/Button.tsx
+++ front/src/components/commons/Button.tsx
@@ -1,6 +1,7 @@
 import styles from '@/styles/Button.module.css';
+import { ButtonProps } from '@/types/button';
 
-function Button({ children, bgc = 'filled' }) {
+function Button({ children, bgc = 'filled' }: ButtonProps) {
   return (
     <>
       <button className={`FontM ${styles.box} ${styles[bgc]}`}>{children}</button>
