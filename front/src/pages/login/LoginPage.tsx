5722c72313fa3467970ae0bc5f64ef69250b3ac4
안준선 <joonsun1006@naver.com>
2024-03-18 14:53:53 +0900

    feat: login err

--- front/src/pages/login/LoginPage.tsx
+++ front/src/pages/login/LoginPage.tsx
@@ -5,11 +5,36 @@ import { Axios } from '@/services/axios';
 import styles from '../../styles/login/LoginPage.module.css';
 
 function LoginPage() {
-  const loginHandler = () => {
-    // const response = Axios.post('api/users/oauth/kakao')
-    const response = Axios.post('/posts');
-    console.log(response);
-    console.log('카카오로 로그인하기');
+  const KAKAO_OAUTH_URL = import.meta.env.VITE_KAKAO_OAUTH_URL;
+  const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
+  const KAKAO_LOGIN_REDIRECT_URI = import.meta.env.VITE_KAKAO_LOGIN_REDIRECT_URI;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_LOGIN_REDIRECT_URI}&response_type=code`;
+
+  const code = new URLSearchParams(window.location.search).get('code');
+  // const code = window.location.search;
+  const loginHandler = async () => {
+    window.location.href = kakaoURL;
+
+
+    const response = await Axios
+      .post('api/users/login', {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json;charset=utf-8', //json형태로 데이터를 보내겠다는뜻
+          'Access-Control-Allow-Origin': '*', //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
+        },
+        params: {
+          code: code,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        // navigate("/home");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // console.log(response.data);
   };
 
   return (
--- front/src/services/axios.tsx
+++ front/src/services/axios.tsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-  // baseURL: 'http://j10c108.p.ssafy.io:3000',
-  // dunmmy
-  baseURL: 'https://jsonplaceholder.typicode.com/',
+  baseURL: 'http://j10c108.p.ssafy.io',
 });
