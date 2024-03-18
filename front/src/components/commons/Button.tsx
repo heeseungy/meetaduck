119f82e9a8816593408164307e46548587977097
안준선 <joonsun1006@naver.com>
2024-03-18 15:42:36 +0900

    feat: 참여하기, 만들기 이벤트 감지

--- front/src/components/commons/Button.tsx
+++ front/src/components/commons/Button.tsx
@@ -1,11 +1,11 @@
 import styles from '@/styles/commons/Button.module.css';
 import { ButtonProps } from '@/types/button';
 
-function Button({ children, bgc = 'filled' }: ButtonProps) {
+function Button({ onClickHandler, children, bgc = 'filled' }: ButtonProps) {
   return (
-    <>
-      <button className={`FontM ${styles.box} ${styles[bgc]}`}>{children}</button>
-    </>
+    <button onClick={onClickHandler} className={`FontM ${styles.box} ${styles[bgc]}`}>
+      {children}
+    </button>
   );
 }
 
--- front/src/pages/party/PartyPage.tsx
+++ front/src/pages/party/PartyPage.tsx
@@ -4,6 +4,14 @@ import Input from '@/components/commons/Input';
 import styles from '@/styles/party/Partyjoin.module.css';
 
 function PartyPage() {
+  const joinHander = () => {
+    console.log('참여하기 클릭!');
+  };
+
+  const createHander = () => {
+    console.log('이번엔 만들기 클릭!');
+  };
+
   return (
     <div className={styles.container}>
       <img src={duckLogo} alt="duckLogo" className={styles.marginTop} />
@@ -12,10 +20,14 @@ function PartyPage() {
         <Input />
       </div>
       <div className={styles.marginTop}>
-        <Button bgc="filled">참여하기</Button>
+        <Button bgc="filled" onClickHandler={joinHander}>
+          참여하기
+        </Button>
       </div>
       <div className={styles.noPartySection}>파티가 없으신가요?</div>
-      <Button bgc="empty">만들기</Button>
+      <Button bgc="empty" onClickHandler={createHander}>
+        만들기
+      </Button>
     </div>
   );
 }
