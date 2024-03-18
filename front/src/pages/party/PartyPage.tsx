9ae8f9cc04e607102621496887d25e6603d9ae2d
안준선 <joonsun1006@naver.com>
2024-03-18 16:18:03 +0900

    feat: party join code input.

--- front/src/pages/party/PartyPage.tsx
+++ front/src/pages/party/PartyPage.tsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
+
 import duckLogo from '@/assets/images/RubberDuckWithLogo.png';
 import Button from '@/components/commons/Button';
 import Input from '@/components/commons/Input';
 import styles from '@/styles/party/Partyjoin.module.css';
 
 function PartyPage() {
-  const joinHander = () => {
+  const [usersInput, setUsersInput] = useState('');
+
+  const joinHandler = () => {
     console.log('참여하기 클릭!');
   };
 
-  const createHander = () => {
+  const createHandler = () => {
     console.log('이번엔 만들기 클릭!');
+  };
+
+  const handleInputChange = (value: string) => {
+    setUsersInput(value);
   };
 
   return (
@@ -17,15 +25,16 @@ function PartyPage() {
       <img src={duckLogo} alt="duckLogo" className={styles.marginTop} />
       <div className={`FontBasic FontL ${styles.joinTitle} `}>파티 참여 코드</div>
       <div className={styles.marginTop}>
-        <Input />
+        <Input usersInput={usersInput} onChange={handleInputChange} />
       </div>
       <div className={styles.marginTop}>
-        <Button bgc="filled" onClickHandler={joinHander}>
+        <Button bgc="filled" onClickHandler={joinHandler}>
           참여하기
         </Button>
       </div>
+
       <div className={styles.noPartySection}>파티가 없으신가요?</div>
-      <Button bgc="empty" onClickHandler={createHander}>
+      <Button bgc="empty" onClickHandler={createHandler}>
         만들기
       </Button>
     </div>
