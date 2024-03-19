1bf8952b8375d63acc8147f67da39ee7ddb82363
안준선 <joonsun1006@naver.com>
2024-03-19 12:18:49 +0900

    feat: date picker

--- front/src/components/party/DatePickerInput.tsx
+++ front/src/components/party/DatePickerInput.tsx
@@ -2,26 +2,27 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
+import dayjs, { Dayjs } from 'dayjs';
+
 function DatePickerInput({ children }) {
-  const [recommendForm, setRecommendForm] = useRecoilState(recommendFormState);
+  // const [recommendForm, setRecommendForm] = useRecoilState(recommendFormState);
 
   const datePickerFormat = 'YYYY-MM-DD';
-  const datePickerUtils = {
-    format: datePickerFormat,
-    parse: (value) => dayjs(value, datePickerFormat, true).toDate(),
-  };
+  // const datePickerUtils = {
+  //   format: datePickerFormat,
+  //   parse: (value) => dayjs(value, datePickerFormat, true).toDate(),
+  // };
 
-  const endDateChange = (date) => {
-    const formattedDate = dayjs(date).format(datePickerFormat);
-    setRecommendForm((prev) => ({
-      ...prev,
-      endDate: formattedDate,
-    }));
-  };
+  // const endDateChange = (date) => {
+  //   const formattedDate = dayjs(date).format(datePickerFormat);
+  //   setRecommendForm((prev) => ({
+  //     ...prev,
+  //     endDate: formattedDate,
+  //   }));
+  // };
 
   return (
     <>
-      <h1>Date Picker Input</h1>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker
           label="종료 날짜를 선택해주세요"
@@ -31,9 +32,12 @@ function DatePickerInput({ children }) {
             },
           }}
           format="YYYY / MM / DD"
-          value={endDate}
-          onChange={(newValue) => {
-            endDateChange(newValue);
+          // value={endDate}
+          // onChange={(newValue) => {
+          //   endDateChange(newValue);
+          // }}
+          shouldDisableDate={(day) => {
+            return dayjs(dayjs(day as Dayjs).format(`YYYY-MM-DD`)).isBefore(`2024-03-19`);
           }}
         />
       </LocalizationProvider>
