6a2f7bae888542047d5d32dfebb853f69e56e689
안준선 <joonsun1006@naver.com>
2024-03-19 09:24:27 +0900

    refactor: login then catch to try catch

--- front/src/services/loginService.ts
+++ front/src/services/loginService.ts
@@ -1,22 +1,19 @@
 import { Axios } from "./axios";
 
 export async function loginService(code: string) { //code: string
-  const response = await Axios.post('api/users/login', {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json;charset=utf-8', //json형태로 데이터를 보내겠다는뜻
-      'Access-Control-Allow-Origin': '*', //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
-    },
-    params: {
-      code: code,
-    },
-  })
-    .then((res) => {
-      console.log(res);
-      // navigate("/home");
+  try {
+    const response = await Axios.post('api/users/login', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=utf-8', //json형태로 데이터를 보내겠다는뜻
+        'Access-Control-Allow-Origin': '*', //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
+      },
+      params: {
+        code: code,
+      },
     })
-    .catch((error) => {
-      console.log(error);
-    });
-  // console.log(response.data);
+    // console.log(response.data);
+  } catch (err) {
+    console.log(err);
+  }
 }
