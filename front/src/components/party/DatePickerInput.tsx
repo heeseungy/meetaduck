66124eed4330a7fb8c49c9e2fa30bb7b515fc0fe
안준선 <joonsun1006@naver.com>
2024-03-19 12:00:35 +0900

    feat: create date picker

--- front/src/components/party/DatePickerInput.tsx
+++ front/src/components/party/DatePickerInput.tsx
@@ -3,13 +3,40 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 function DatePickerInput({ children }) {
+  const [recommendForm, setRecommendForm] = useRecoilState(recommendFormState);
+
+  const datePickerFormat = 'YYYY-MM-DD';
+  const datePickerUtils = {
+    format: datePickerFormat,
+    parse: (value) => dayjs(value, datePickerFormat, true).toDate(),
+  };
+
+  const endDateChange = (date) => {
+    const formattedDate = dayjs(date).format(datePickerFormat);
+    setRecommendForm((prev) => ({
+      ...prev,
+      endDate: formattedDate,
+    }));
+  };
+
   return (
     <>
       <h1>Date Picker Input</h1>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker />
+        <DatePicker
+          label="종료 날짜를 선택해주세요"
+          slotProps={{
+            textField: {
+              size: 'small',
+            },
+          }}
+          format="YYYY / MM / DD"
+          value={endDate}
+          onChange={(newValue) => {
+            endDateChange(newValue);
+          }}
+        />
       </LocalizationProvider>
-      
     </>
   );
 }
