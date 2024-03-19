49bfcb5e00cb9c04ba26bb965d89781e75839c08
안준선 <joonsun1006@naver.com>
2024-03-19 17:58:54 +0900

    feat: chatting styles

--- front/src/pages/chatting/ChattingPage.tsx
+++ front/src/pages/chatting/ChattingPage.tsx
@@ -1,7 +1,18 @@
+import Card from '@/components/commons/Card';
+import styles from '@/styles/chatting/ChattingPage.module.css';
+
 function ChattingPage() {
+  const children = (
+    <div className={styles.cardMargin}>
+      I am Card Contents
+    </div>
+  );
   return (
     <>
-      <h1>ChattingPage</h1>
+      <h1 className={`FontBasic FontL ${styles.heading}`}>채팅</h1>
+      <div className={styles.cardContainer}>
+        <Card {...{ tag: 2, children: children }} />
+      </div>
     </>
   );
 }
