c0b0cd93ae56d87f0e35a22a12c878f9ee7b1055
최지웅 <jwchoi42@naver.com>
2024-03-20 17:27:48 +0900

    fix: 카카오 로그인 API에서 partyId랑 guestId 정보도 주도록 수정

--- back/src/main/java/com/ssafy/duck/domain/guest/repository/GuestRepository.java
+++ back/src/main/java/com/ssafy/duck/domain/guest/repository/GuestRepository.java
@@ -16,6 +16,10 @@ public interface GuestRepository extends JpaRepository<Guest, Long> {
             "WHERE guest.party.partyId = :partyId")
     List<Guest> findAllByPartyId(Long partyId);
 
+    @Query("SELECT guest FROM Guest guest " +
+            "WHERE guest.user.userId = :userId")
+    Optional<Guest> findByUserId(Long userId);
+
     List<Guest> findByParty_PartyId(Long partyId);
 
     Optional<Guest> findByUser_UserId(Long userId);
