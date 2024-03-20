9231f966ac68d439ac7ee96d0af36fbf68237fe7
최지웅 <jwchoi42@naver.com>
2024-03-20 10:17:38 +0900

    feat: 시작 전 참가자 목록 조회

--- back/src/main/java/com/ssafy/duck/domain/guest/controller/GuestController.java
+++ back/src/main/java/com/ssafy/duck/domain/guest/controller/GuestController.java
@@ -1,11 +1,14 @@
 package com.ssafy.duck.domain.guest.controller;
 
 import com.ssafy.duck.domain.guest.dto.request.VoteReq;
+import com.ssafy.duck.domain.guest.dto.response.GuestRes;
 import com.ssafy.duck.domain.guest.dto.response.VoteRes;
 import com.ssafy.duck.domain.guest.service.GuestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/guests")
@@ -14,10 +17,16 @@ public class GuestController {
 
     private final GuestService guestService;
 
+    @GetMapping("/all/{partyId}")
+    public ResponseEntity<List<GuestRes>> getAllByPartyId(@PathVariable Long partyId) {
+        List<GuestRes> guestList = guestService.findAllByPartyId(partyId);
+        return ResponseEntity.ok(guestList);
+    }
+
     @PatchMapping
     public ResponseEntity<VoteRes> patchVote(@RequestBody VoteReq voteReq) {
         VoteRes voteRes = guestService.vote(voteReq);
-        return ResponseEntity.ok().body(voteRes);
+        return ResponseEntity.ok(voteRes);
     }
 
     @DeleteMapping("/{guestId}")
