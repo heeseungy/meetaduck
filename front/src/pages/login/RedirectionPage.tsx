aed452538487b3c2d1db5d4d469315da0769ecd8
안준선 <joonsun1006@naver.com>
2024-03-20 16:00:49 +0900

    feat: login logic

--- front/src/pages/login/RedirectionPage.tsx
+++ front/src/pages/login/RedirectionPage.tsx
@@ -1,20 +1,39 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { loginService } from '@/services/loginService';
+import { Axios } from '@/services/axios';
 
 function RedirectionPage() {
-  const code = new URLSearchParams(window.location.search).get('code');
+  const code: string = new URLSearchParams(window.location.search).get('code')!;
+  const navigate = useNavigate();
 
   useEffect(() => {
+    Axios.get('api/users/login', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=utf-8', //json형태로 데이터를 보내겠다는뜻
+        'Access-Control-Allow-Origin': '*', //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
+      },
+      params: {
+        code: code,
+      },
+    })
+      .then((response) => {
+        console.log('responsessss : ', response);
+        navigate('/party');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // navigate('/party');
     // console.log(process.env.REACT_APP_URL);
-    if (code) {
-      loginService(code);
-    } else {
-      console.log('code 없음');
-    }
+    // if (code) {
+    //   loginService(code);
+    // } else {
+    //   console.log('code 없음');
+    // }
   }, []);
-
-  return <div>로그인 중입니다.</div>;
+  // return <div>로그인 중입니다.</div>;
 }
 
 export default RedirectionPage;
--- front/src/services/axios.ts
+++ front/src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-  baseURL: 'http://j10c108.p.ssafy.io',
+  baseURL: 'http://j10c108.p.ssafy.io:8080',
 });
