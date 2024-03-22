d95a090fb0b88f6874bb5ad0c74284643b9c69af
강지수 <pwindx20@gmail.com>
2024-03-22 17:55:46 +0900

    chore: navigation CSS
    진행중

--- front/src/components/navbar/MainNav.tsx
+++ front/src/components/navbar/MainNav.tsx
@@ -1,31 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { ChartBar, ChatCircleDots, FlagBanner, Lightbulb } from '@phosphor-icons/react';
 
 import classes from '../../styles/MainNav.module.css';
 
 function MainNav() {
+  const location = useLocation();
+  console.log(location);
   return (
     <>
       <nav className={classes.wrapper}>
         <div>
           <Link to="/mission">
-            <FlagBanner size={32} />
+            <FlagBanner size={32} color="#4d4637" />
           </Link>
         </div>
         <div>
           <Link to="/chatting">
-            <ChatCircleDots size={32} />
+            <ChatCircleDots size={32} color="#4d4637" />
           </Link>
         </div>
         <div>
           <Link to="/hint">
-            <Lightbulb size={32} />
+            <Lightbulb size={32} color="#4d4637" />
           </Link>
         </div>
         <div>
           <Link to="/result">
-            <ChartBar size={32} />
+            <ChartBar size={32} color="#4d4637" />
           </Link>
         </div>
       </nav>
