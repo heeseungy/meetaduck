3d5f44cb3bec53d2f94dae32c40ab520d0835d11
강지수 <pwindx20@gmail.com>
2024-03-23 19:46:15 +0900

    design: nav component CSS 수정

--- front/src/components/navbar/MainNav.tsx
+++ front/src/components/navbar/MainNav.tsx
@@ -2,32 +2,60 @@ import { Link, useLocation } from 'react-router-dom';
 
 import { ChartBar, ChatCircleDots, FlagBanner, Lightbulb } from '@phosphor-icons/react';
 
-import classes from '../../styles/MainNav.module.css';
+import styles from '../../styles/MainNav.module.css';
 
 function MainNav() {
   const location = useLocation();
   console.log(location);
   return (
     <>
-      <nav className={classes.wrapper}>
+      <nav className={styles.wrapper}>
         <div>
           <Link to="/mission">
-            <FlagBanner size={32} color="#4d4637" />
+            {location.pathname === '/mission' ? (
+              <>
+                <div className={`${styles.BarMission} ${styles.UnderBar}`}></div>
+                <FlagBanner size={32} weight="fill" color="#ffd656" />
+              </>
+            ) : (
+              <FlagBanner size={32} color="#4d4637" />
+            )}
           </Link>
         </div>
         <div>
           <Link to="/chatting">
-            <ChatCircleDots size={32} color="#4d4637" />
+            {location.pathname === '/chatting' ? (
+              <>
+                <div className={`${styles.BarChatting} ${styles.UnderBar}`}></div>
+                <ChatCircleDots size={32} weight="fill" color="#ffd656" />
+              </>
+            ) : (
+              <ChatCircleDots size={32} color="#4d4637" />
+            )}
           </Link>
         </div>
         <div>
           <Link to="/hint">
-            <Lightbulb size={32} color="#4d4637" />
+            {location.pathname === '/hint' ? (
+              <>
+                <div className={`${styles.BarHint} ${styles.UnderBar}`}></div>
+                <Lightbulb size={32} weight="fill" color="#ffd656" />
+              </>
+            ) : (
+              <Lightbulb size={32} color="#4d4637" />
+            )}
           </Link>
         </div>
         <div>
           <Link to="/result">
-            <ChartBar size={32} color="#4d4637" />
+            {location.pathname === '/result' ? (
+              <>
+                <div className={`${styles.BarResult} ${styles.UnderBar}`}></div>
+                <ChartBar size={32} weight="fill" color="#ffd656" />
+              </>
+            ) : (
+              <ChartBar size={32} color="#4d4637" />
+            )}
           </Link>
         </div>
       </nav>
