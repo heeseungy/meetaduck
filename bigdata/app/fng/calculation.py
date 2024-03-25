c158e6db4763bb6af38f320e3421b06b7dd4dd4a
김현영 <hggew@naver.com>
2024-03-25 15:08:26 +0900

    feat: lfs 해제

---
+++ bigdata/app/fng/calculation.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9ef05d99228ab116913e1d541f61bdf1a3eed2bdd36bd3607eb39b67d99e973
+size 5391
---
+++ bigdata/app/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fc44a0c87a7a20b2db02c36ece66f6a775eee3e1710be86969566a7ee6a0e3e5
+size 8363
