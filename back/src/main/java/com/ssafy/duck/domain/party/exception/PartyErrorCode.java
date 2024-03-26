802da25f9a743dcae4765bfc69f281f6a4733cdc
양희승 <heeseung0223@naver.com>
2024-03-26 10:10:52 +0900

    feat: 파티 시작 시 예외처리
    1) 종료일자가 현재일자보다 빠른 경우
    2) 게스트 수가 2명 이하인 경우

--- back/src/main/java/com/ssafy/duck/domain/party/exception/PartyErrorCode.java
+++ back/src/main/java/com/ssafy/duck/domain/party/exception/PartyErrorCode.java
@@ -12,9 +12,11 @@ public enum PartyErrorCode {
     MAXIMUM_OF_1_PARTY_ALLOWED(Status.BAD_REQUEST, "파티는 최대 1개만 생성 할 수 있습니다."),
     MAXIMUM_OF_1_PARTY_JOINED(Status.BAD_REQUEST, "파티는 최대 1개만 가입 할 수 있습니다."),
     MAXIMUM_OF_7_DAYS_ALLOWED(Status.BAD_REQUEST, "진행 기간은 최대 7일입니다."),
+    THE_TIME_IS_SET_INCORRECTLY(Status.BAD_REQUEST, "파티 종료 시간은 현재 시간보다 이후여야 합니다."),
+    NOT_ENOUGH_PEOPLE(Status.BAD_REQUEST, "파티 최소 인원은 3명 입니다."),
+    PARTY_IS_IN_PROGRESS(Status.BAD_REQUEST, "아직 파티가 진행 중 입니다."),
     NOT_FOUND_PARTY(Status.NOT_FOUND, "찾을 수 없는 파티입니다."),
-    ACCESS_DENIED(Status.FORBIDDEN, "해당 파티에 권한이 없는 유저입니다."),
-    PARTY_IS_IN_PROGRESS(Status.BAD_REQUEST, "아직 파티가 진행 중 입니다.");
+    ACCESS_DENIED(Status.FORBIDDEN, "해당 파티에 권한이 없는 유저입니다.");
 
     private final Status status;
     private final String detail;
