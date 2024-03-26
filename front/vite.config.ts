0db54e15c73b08699a0be23408067e2655eab740
최지웅 <jwchoi42@naver.com>
2024-03-26 23:49:14 +0900

    fix: *.p12, *.pem git ignore 추가

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,10 @@ export default defineConfig({
     'global': {},
   },
   server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),  
+    },
     host: '0.0.0.0',
     port: 3000,
   },
