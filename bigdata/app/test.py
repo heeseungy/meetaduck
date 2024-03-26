478bfeffa0f7438d6a14cd8061d7c2ad697937e9
김현영 <hggew@naver.com>
2024-03-27 00:47:41 +0900

    feat: 모델 수정

--- bigdata/app/test.py
+++ bigdata/app/test.py
@@ -14,7 +14,7 @@ model.load_state_dict(torch.load(koelectra_finetuned_model_dir, map_location=tor
 sentiment_classifier = pipeline('sentiment-analysis', tokenizer=tokenizer, model=model)
 
 #상수
-STANDARD_POINT = 50
+STANDARD_POINT = 300
 STANDARD_PERIOD =120
 
 # 긍정어 비율 계산
@@ -42,27 +42,9 @@ def predict_sentiment(content, point, isMe):
 
 
 def make_dt(time_str):
-    # 정규 표현식을 사용하여 마이크로초를 추출
-    microseconds_match = re.search(r'\.\d+', time_str)
-    microseconds_str = microseconds_match.group()[1:]  # '.'을 제외한 숫자 부분만 가져옴
-
-    # 마이크로초의 길이가 6자리 이상이면 초당 증가하는 마이크로초가 아니므로 뒤에 0을 추가하여 6자리로 만듭니다.
-    if len(microseconds_str) > 6:
-        microseconds_str = microseconds_str[:6]
-
-    # 마이크로초를 int 형으로 변환
-    microseconds = int(microseconds_str)
-
-    # 마이크로초를 제외한 시간 문자열 생성
-    time_str_without_microseconds = re.sub(r'\.\d+', '', time_str)
-
-    # 시간 문자열을 datetime 객체로 변환
-    time = dt.datetime.strptime(time_str_without_microseconds, "%Y-%m-%dT%H:%M:%SZ")
-
-    # datetime 객체에 마이크로초 추가 (범위 내로 조정)
-    time = time.replace(microsecond=min(microseconds, 999999))
+    # ISO 8601 형식의 시간 문자열을 파싱하여 datetime 객체로 변환
+    time = dt.datetime.fromisoformat(time_str)
     return time
-
 
 
 #시간과 점수를 함께 다루기 위한 객체
@@ -107,7 +89,7 @@ def calc_favorability(guest_id, chat_list):
                 if len(time_block) == 0:
                     point = STANDARD_POINT
                 else:
-                    point = time_block[len(time_block)-1][len(time_block[len(time_block)-1])-1].point
+                    point = time_block[len(time_block)-1][len(time_block[len(time_block)-1])-1].point           
             
             point = predict_sentiment(chat_message['content'], point, chat_message['sender_id'] ==guest_id)
             time_block.append([TimeAndPoint(compare_time, point)])
@@ -122,14 +104,13 @@ def calc_favorability(guest_id, chat_list):
     
 
     if len(time_block[len(time_block)-1]) == 1:
-        time_block.pop()
+        time_block.pop()    
+
     #총시간, 회화당 시간 계산
-    total_time = 0
-    block_time = []
+    total_time = 0 
 
     for oneblock in time_block:
         time_delta =(oneblock[len(oneblock)-1].time- oneblock[0].time).total_seconds()
-        block_time.append(time_delta)
         total_time += time_delta
 
     #기준시간 간격
@@ -140,26 +121,33 @@ def calc_favorability(guest_id, chat_list):
     checked_idx = 0
 
     for oneblock in time_block:
+        number_of_period = 0
         for chat  in oneblock:
-          loop_is_done = False
-          while(not loop_is_done):
-              if chat.time >= oneblock[0].time + dt.timedelta(seconds=(period * checked_idx)) and chat.time < oneblock[0].time + dt.timedelta(seconds=(period * (checked_idx + 1))):
-                preprocessed_data[checked_idx].append(chat.point)
-                loop_is_done =True
-              else:
-                  preprocessed_data.append([])
-                  checked_idx += 1
+            loop_is_done = True
+            while(loop_is_done):
+                if chat.time >= oneblock[0].time + dt.timedelta(seconds=(period * number_of_period)) and chat.time < oneblock[0].time + dt.timedelta(seconds=(period * (number_of_period + 1))):
+                    preprocessed_data[checked_idx].append(chat.point)
+                    loop_is_done = False
+                else:
+                    preprocessed_data.append([])
+                    number_of_period += 1
+                    checked_idx += 1
+        preprocessed_data.append([])
         checked_idx += 1
+    
+    if(len(preprocessed_data[len(preprocessed_data)-1]) == 0):
+        preprocessed_data.pop()
 
 
     #함수에 넣을 p(기간 내 마지막 point), h(기간 내 가장 높은 point), l(기간 내 가장 적은 point), v(기간 내 회화 빈도)
-    p = [50]
-    h = [50]
-    l = [50]
-    v = [0]
+    p = []
+    h = []
+    l = []
+    v = []
 
     #종가를 정하기 위한 변수       
-    last = preprocessed_data[0][0] 
+    last = preprocessed_data[0][0]
+
     for interval in preprocessed_data:
         count = 0
         min = last
@@ -174,14 +162,14 @@ def calc_favorability(guest_id, chat_list):
         p.append(last)
         h.append(max)
         l.append(min)
-        v.append(count) 
+        v.append(count)
+
     dur= len(p)
 
     p=np.array([p])
     h=np.array([h])
     l=np.array([l])
     v=np.array([v]) 
-    score = scoreStock(p,h,l,v)
     print(p, h, l, v)
 
     score_fng = FearGreed(score).compute_stock(duration=(dur-2)) 
