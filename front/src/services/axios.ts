7b546cdfd4ebe4cb378b0309985e05a21ac69914
양희승 <heeseung0223@naver.com>
2024-03-29 02:05:20 +0900

    fix: axios header 추가

--- front/src/services/axios.ts
+++ front/src/services/axios.ts
@@ -2,5 +2,10 @@ import axios from 'axios';
 
 export const Axios = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json;charset=utf-8',
+    'Access-Control-Allow-Origin': '*'
+  },
 });
