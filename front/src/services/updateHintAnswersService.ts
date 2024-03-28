a773cf7b730b394957b44ccc2f5f311fefe60e41
안준선 <joonsun1006@naver.com>
2024-03-28 17:47:45 +0900

    feat: hint input form send done

--- front/src/services/updateHintAnswersService.ts
+++ front/src/services/updateHintAnswersService.ts
@@ -1,19 +1,14 @@
 import { useEffect, useState } from 'react';
 import { Axios } from './axios';
 
-export async function updateHintAnswersService(guestId, { hintId, hintStatusAnswer }) {
+export async function updateHintAnswersService(guestId, hintData ) {
   console.log("guestId:", guestId)
-  console.log("hintId:", hintId)
-  console.log("hintStatusAnswer:", hintStatusAnswer)
+  console.log("hintData123:", hintData)
 
   try {
-    await Axios.patch(`/api/hints/${guestId}`, {
-      hindId: hintId,
-      hintStatusAnswer: hintStatusAnswer,
-    });
+    await Axios.patch(`/api/hints/${guestId}`, hintData);
     console.log('힌트 답변이 성공적으로 업데이트되었습니다.');
   } catch (err) {
-    alert('오류!')
     console.error('힌트 답변 업데이트 중 오류 발생:', err);
     throw err;
   }
