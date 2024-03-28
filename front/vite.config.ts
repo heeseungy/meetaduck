2699e0b1a37981286552e5eb69c5158a474d75da
최지웅 <jwchoi42@naver.com>
2024-03-28 16:50:37 +0900

    chore: vite.config pem 삭제

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -9,11 +9,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 3000,
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem'),
-    },
+    port: 3000
   },
 
   plugins: [react()],
