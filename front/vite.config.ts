b0a076ed63fb87c006319f52bb9842b4f32f0425
최지웅 <jwchoi42@naver.com>
2024-03-28 09:46:16 +0900

    Update front/vite.config.ts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    https: {
+      key: fs.readFileSync('./j10c108.p.ssafy.io-key.pem'),
+      cert: fs.readFileSync('./j10c108.p.ssafy.io.pem')
+    }
   },
 
   plugins: [react()],
