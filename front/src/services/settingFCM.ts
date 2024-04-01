0b180a3f24d35094bfcc06c60009e19fe7a5bb82
강지수 <pwindx20@gmail.com>
2024-04-01 15:44:25 +0900

    fix: chatting inputbox preview height

---
+++ front/src/services/settingFCM.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+import 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
