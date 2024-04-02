2e3aa97fc64f86dbd8e4e9def2dac0c9d99dc46c
강지수 <pwindx20@gmail.com>
2024-04-02 23:28:38 +0900

    fix: 미션 수정

--- front/src/pages/mission/MissionCompletePage.tsx
+++ front/src/pages/mission/MissionCompletePage.tsx
@@ -22,7 +22,8 @@ function MissionCompletePage({ role, party, nickname, missionResultList }: Missi
   const startTime = new Date(party.startTime);
   const endTime = new Date(party.endTime);
   // const date = mission.length;
-  const date = Math.ceil(Math.abs((endTime.getTime() - startTime.getTime()) / (24 * 60 * 1000)));
+  // const date = Math.ceil(Math.abs((endTime.getTime() - startTime.getTime()) / (24 * 60 * 1000)));
+  const date = Math.ceil(Math.abs((endTime.getTime() - startTime.getTime()) / (3 * 60 * 1000) - 1));
   console.log('mission complete date', date);
   let days: string[] = [];
   for (let i = 0; i < date; i++) {
